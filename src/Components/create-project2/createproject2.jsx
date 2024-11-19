07f4d6954e22561cd2e6d769e086388383df2cc4
ahmeddhaferr <ahmedalkurdi485@gmail.com>
2024-11-20 00:46:56 +0300

    latest

--- src/Components/create-project2/createproject2.jsx
+++ src/Components/create-project2/createproject2.jsx
@@ -8,6 +8,25 @@ import Crosswhite from "../../assets/crosswhite.svg";
 
 export default function createproject2() {
   const toggleProj = useStore((state) => state.toggleProj);
+  const [colorchange, setColorchange] = useState(false);
+  const [colorchange1, setColorchange1] = useState(false);
+  const [modalpop, setModalpop] = useState(false);
+  const [modalpop1, setModalpop1] = useState(false);
+
+  const handlebutton = () => {
+    setColorchange(true)
+    setColorchange1(false)
+    setModalpop(true)
+    setModalpop1(false)
+  };
+  const handlebutton1 = () => {
+    setColorchange1(true)
+    setColorchange(false)
+    setModalpop(false)
+    setModalpop1(true)
+  };
+
+
 
   return (
     <>
@@ -25,8 +44,32 @@ export default function createproject2() {
         <div>
           <Stepper1 />
         </div>
-        <div>
-            
+        <div className="underr-stepper">
+          <p>Project Duration</p>
+          <input type="text" placeholder="Duration of the project" />
+          <p id="m16">Pricing Type</p>
+          <div className="fondos">
+            <button
+              className={`${colorchange ? "colorado" : "fondos-button"}`}
+              onClick={handlebutton}
+            >
+              Per Hour
+            </button>
+            <button 
+             className={`${colorchange1 ? "colorado" : "fondos-button"}`}
+             onClick={handlebutton1}
+            >For The Project</button>
+          </div>
+          <div>
+          {modalpop && (<div className="homies">
+  <p>Price Per Hour</p>
+  <input type="text" placeholder="Enter Price"/>
+</div>)}
+          {modalpop1 && (<div className="homies">
+  <p>Price For The Project</p>
+  <input type="text" placeholder="Enter Price"/>
+</div>)}
+</div>
         </div>
       </div>
     </>
